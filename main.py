af7c5e76735eb929b69597742fbc53e07f1be1d5
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-12-22 01:23:47 -0600

    WIP

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
-import pyodbc
 import os
+import os.path
+from functools import reduce
+
+import pyodbc
+from google.oauth2 import service_account
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 FIELDS = {
   "vehicle_identification_number": "VARCHAR(17) UNIQUE NOT NULL",
@@ -23,6 +32,7 @@ FIELDS = {
   "stock_number":                  "INT NOT NULL PRIMARY KEY IDENTITY(1, 1)",
   "notes":                         "VARCHAR(256)",
 }
+
 
 class colors:
   HEADER = '\033[95m'
@@ -50,6 +60,12 @@ connectionString = (
   f'UID={USERNAME};' +
   f'PWD={PASSWORD}')
 
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SERVICE_ACCOUNT_FILE = './credentials.json'
+
+SPREADSHEET_ID = os.getenv("GOODLE_SHEET_ID")
+RANGE = "A2:Q157"
+
 
 def create_table(cursor, table_name, fields):
   print(f'{colors.OKBLUE}[[INFO]]: attempting to create table "{table_name}"{colors.ENDC}')
@@ -66,6 +82,22 @@ def create_table(cursor, table_name, fields):
     return None
 
   print(f'{colors.OKBLUE}[[INFO]]: successfuly created table "{table_name}"{colors.ENDC}')
+  return cursor
+
+def insert_rows_into_table(cursor, table_name, rows):
+  print(f'{colors.OKBLUE}[[INFO]]: attempting to insert data into table "{table_name}"{colors.ENDC}')
+  query = (
+    f"INSERT INTO {table_name} VALUES "+
+    f"{', '.join([str(tuple(r)) for r in rows])}" )
+
+  try:
+    cursor.execute(query).commit()
+  except Exception as e:
+    print(f'{colors.FAIL}[[ERROR]]: while attempting to insert data into table {table_name}.{colors.ENDC}')
+    print(f'\t{colors.FAIL}{e}{colors.ENDC}')
+    return None
+
+  print(f'{colors.OKBLUE}[[INFO]]: successfuly inserted data into table "{table_name}"{colors.ENDC}')
   return cursor
 
 
@@ -103,6 +135,61 @@ def open_connection():
   return conn
   
 
+def get_data():
+  creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+  values = []
+  try:
+    service = build("sheets", "v4", credentials=creds)
+    sheet = service.spreadsheets()
+    values = (
+      sheet.values()
+           .get(spreadsheetId=SPREADSHEET_ID, range=RANGE)
+           .execute()
+           .get("values", []) )
+  except HttpError as err:
+    print(err)
+    return None
+
+  return values
+
+
+def normalize_number(s):
+  return s.strip().replace(',', '')
+
+
+TRANSFORMS = {
+  4: [normalize_number],
+  10: [
+    lambda s: s.replace('CVT', 'Automatic'),
+  ],
+  11: [
+    lambda s: s.replace('Four-Wheel Drive', '4WD'),
+    lambda s: s.replace('All-Wheel Drive', 'AWD'),
+    lambda s: s.replace('Rear-Wheel Drive', 'RWD'),
+    lambda s: s.replace('Front-Wheel Drive', 'FWD'),
+  ],
+  12: [
+    lambda s: s.replace('Plug-in Hybrid', 'Hybrid'),
+  ],
+  13: [
+    lambda s: s.replace('Pickup Truck', 'Pickup'),
+  ],
+  16: [
+    lambda s: r''.format(s),
+  ],
+
+}
+
+
+def apply_transforms(transforms, row):
+  return [ 
+    reduce((lambda acc, fn: fn(acc)) ,transforms.get(i, [lambda _:_]), c)
+    for i, c in enumerate(row)
+  ]
+
+
 def main():
   conn = open_connection()
   if conn == None:
@@ -116,6 +203,16 @@ def main():
   cursor = create_table(cursor, INVENTORY_TABLE, FIELDS) if len(table_names) == 0 else cursor
   if cursor == None:
     exit(3)
+
+  rows = get_data()
+  if rows == None:
+    exit(4)
+
+  for i,r in enumerate(rows):
+    print(f'{i} - {len(r)}')
+
+  transformed_rows = [apply_transforms(TRANSFORMS, r)  for r in rows]
+  insert_rows_into_table(cursor, INVENTORY_TABLE, transformed_rows[:])
 
   conn.close()
 
