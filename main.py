176050175193b3122f6f8dd44914e075cbc2918c
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-12-23 09:30:20 -0600

    feat: pull records from google sheets and put into sql database

--- main.py
+++ main.py
@@ -1,123 +1,255 @@
 #!/usr/bin/env python
 
-import pyodbc
-import os
+import pdb
 
-FIELDS = {
-  "vehicle_identification_number": "VARCHAR(17) UNIQUE NOT NULL",
-  "make":                          "VARCHAR(32) NOT NULL",
-  "model":                         "VARCHAR(32) NOT NULL",
-  "year":                          "SMALLINT NOT NULL",
-  "mileage":                       "INT NOT NULL",
-  "price":                         "MONEY NOT NULL",
-  "condition":                     "VARCHAR(32) NOT NULL",
-  "color":                         "VARCHAR(32) NOT NULL",
-  "interior_color":                "VARCHAR(32) NOT NULL",
-  "engine":                        "VARCHAR(32) NOT NULL",
-  "transmission":                  "VARCHAR(32) NOT NULL",
-  "drive_train":                   "VARCHAR(32) NOT NULL",
-  "fuel_type":                     "VARCHAR(32) NOT NULL",
-  "body_style":                    "VARCHAR(32) NOT NULL",
-  "number_of_seats":               "TINYINT NOT NULL",
-  "doors":                         "TINYINT NOT NULL",
-  "stock_number":                  "INT NOT NULL PRIMARY KEY IDENTITY(1, 1)",
-  "notes":                         "VARCHAR(256)",
+import re
+import os
+import os.path
+from functools import reduce
+
+import pyodbc
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, mapped_column, MappedAsDataclass, Mapped
+from sqlalchemy import select, Integer, String, Identity
+from sqlalchemy.dialects.mssql import TINYINT, VARCHAR, SMALLINT, MONEY
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class Base(MappedAsDataclass, DeclarativeBase): pass
+
+class VehicleInventory(Base):
+  __tablename__                 = "vehicle_inventory"
+
+  vehicle_identification_number : Mapped[str] = mapped_column(VARCHAR(17), unique=True, nullable=False)
+  make                          : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  model                         : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  year                          : Mapped[int] = mapped_column(SMALLINT, nullable=False)
+  mileage                       : Mapped[int] = mapped_column(Integer, nullable=False)
+  price                         : Mapped[str] = mapped_column(MONEY, nullable=False)
+  condition                     : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  color                         : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  interior_color                : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  engine                        : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  transmission                  : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  drive_train                   : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  fuel_type                     : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  body_style                    : Mapped[str] = mapped_column(VARCHAR(32), nullable=False)
+  number_of_seats               : Mapped[int] = mapped_column(TINYINT, nullable=False)
+  doors                         : Mapped[int] = mapped_column(TINYINT, nullable=False)
+  stock_number                  : Mapped[int] = mapped_column(Integer, Identity(), init=False, nullable=False,
+                                                              primary_key=True)
+  notes                         : Mapped[str] = mapped_column(VARCHAR(256))
+
+
+TRANSFORMS = {
+  4: [
+    lambda s: s.strip().replace(',', ''),
+  ],
+  10: [
+    lambda s: s.replace('CVT', 'Automatic'),
+  ],
+  11: [
+    lambda s: s.replace('Four-Wheel Drive', '4WD'),
+    lambda s: s.replace('All-Wheel Drive', 'AWD'),
+    lambda s: s.replace('Rear-Wheel Drive', 'RWD'),
+    lambda s: s.replace('Front-Wheel Drive', 'FWD'),
+  ],
+  12: [
+    lambda s: s.replace('Plug-in Hybrid', 'Hybrid'),
+  ],
+  13: [
+    lambda s: s.replace('Pickup Truck', 'Pickup'),
+  ],
+  16: [
+    lambda s: r''.format(s),
+  ],
+  -1: [
+  ]
 }
 
-class colors:
-  HEADER = '\033[95m'
-  OKBLUE = '\033[94m'
-  OKCYAN = '\033[96m'
-  OKGREEN = '\033[92m'
-  WARNING = '\033[93m'
-  FAIL = '\033[91m'
-  ENDC = '\033[0m'
-  BOLD = '\033[1m'
-  UNDERLINE = '\033[4m'
 
-INVENTORY_TABLE = "vehicle_inventory"
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SERVICE_ACCOUNT_FILE = './credentials.json'
+
 SERVER = os.getenv("DB_HOST")
-DATABASE = os.getenv("DB_CONTEXT")
-USERNAME = os.getenv("DB_USER")
-PASSWORD = os.getenv("DB_PASSWORD")
+DB = os.getenv("DB_CONTEXT")
+USER = os.getenv("DB_USER")
+PWD = os.getenv("DB_PASSWORD")
 
-connectionString = (
-  f'DRIVER={{ODBC Driver 18 for SQL Server}};' +
-  f'TrustServerCertificate=no;' +
-  f'SERVER={SERVER},1433;' +
-  f'DATABASE={DATABASE};' +
-  f'encrypt=no;' +
-  f'UID={USERNAME};' +
-  f'PWD={PASSWORD}')
+SPREADSHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+RANGE = os.getenv("GOOGLE_SHEET_RANGE")
 
 
-def create_table(cursor, table_name, fields):
-  print(f'{colors.OKBLUE}[[INFO]]: attempting to create table "{table_name}"{colors.ENDC}')
+def format_log(message, level="info"):
+  class colors:
+    info = '\033[94m'
+    ok = '\033[92m'
+    warn = '\033[93m'
+    err = '\033[91m'
+    endc = '\033[0m'
+
+  match level:
+    case "error":
+      prefix = "[[ERROR]]: "
+      color = colors.err
+    case "warning":
+      prefix = "[[WARNING]]: "
+      color = colors.warn
+    case _:
+      prefix = "[[INFO]]: "
+      color = colors.info
+
+  final_message = f"{color}{prefix}{message}{colors.endc}"
+
+  print(final_message)
+
+
+def create_table(session, table):
+  format_log(f'attempting to create table "{table.name}"')
+
+  try:
+    table.create(session.connection().engine)
+  except Exception as e:
+    format_log(f'while attempting to create table {table.name}.\n\t{e}', "error")
+    return None
+
+  format_log(f'successfuly created table "{table.name}".')
+  return session
+
+
+def find_duplicates(cursor, table_name, field, value):
   query = (
-    f"CREATE TABLE {table_name} ("+
-    ', '.join([' '.join(x) for x in fields.items()]) +
-    ')' )
+    f'SELECT * FROM {table_name} '
+    f'WHERE {field} = "{value}"' )
+
+  rows = cursor.execute(query).fetchall()
+  return rows
+
+
+def insert_rows_into_table(session, table_name, rows):
+  format_log(f'attempting to insert data into table "{table_name}".')
 
   try:
-    cursor.execute(query).commit()
+    for row in rows:
+      vin = row.vehicle_identification_number
+      duplicate = session.scalars(
+          select(VehicleInventory).filter_by(vehicle_identification_number=vin)
+      ).all()
+
+      if not duplicate:
+        session.add(row)
+        session.commit()
+
   except Exception as e:
-    print(f'{colors.FAIL}[[ERROR]]: while attempting to create table {table_name}.{colors.ENDC}')
-    print(f'\t{colors.FAIL}{e}{colors.ENDC}')
+    format_log(
+      f'while attempting to insert data into table {table_name}.\n\t{e}',
+      "error")
+
     return None
 
-  print(f'{colors.OKBLUE}[[INFO]]: successfuly created table "{table_name}"{colors.ENDC}')
-  return cursor
+  format_log(f'successfuly inserted data into table "{table_name}".')
+  return session
 
 
-def check_if_table_exists(cursor, table_name):
-  print(f'{colors.OKBLUE}[[INFO]]: checking if table "{table_name}" exists.{colors.ENDC}')
+def check_if_table_exists(conn, table_name):
+  format_log(f'checking if table "{table_name}" exists.')
 
+  query = f"SELECT name FROM sys.tables WHERE name = '{table_name}'"
   try:
-    res = cursor.execute(f"SELECT name FROM sys.tables WHERE name = '{table_name}'")
+    res = [x for x in conn.execute(sa.text(query)).mappings()]
   except Exception as e: 
-    print(f'{colors.FAIL}[[ERROR]]: while checking existence of table.{colors.ENDC}')
-    print(f'\t{colors.FAIL}{e}{colors.ENDC}')
+    format_log(f'while checking existence of table.\n\t{e}', "error")
     return None
- 
-  table_names = res.fetchone()
 
-  if table_names is None:
-    print(f'{colors.OKBLUE}[[INFO]]: table "{table_name}" does not exist.{colors.ENDC}')
-    return []
-  else:
-    print(f'{colors.OKBLUE}[[INFO]]: table already "{table_name}" exists.{colors.ENDC}')
-    return table_names
-    
+  msg = f'table already "{table_name}" exists.'
+  if not res:
+    msg = f'table "{table_name}" does not exist.'
+    res = []
 
-def open_connection():
-  print(f'{colors.OKBLUE}[[INFO]]: attempting database connection using connection string:\n{colors.ENDC}'+
-        f'\t{colors.OKBLUE}{connectionString}...{colors.ENDC}')
+  format_log(msg)
+  return res
+
+
+def start_session(server, db, user, pwd):
+  format_log('Starting new database session.')
+
+  connectionString = (
+    f'mssql+pyodbc://{USER}:{PWD}@{SERVER}/{DB}'
+    f'?driver={pyodbc.drivers()[0].replace(" ", "+")}'
+     '&TrustServerCertificate=no&encrypt=no')
+
+  format_log(connectionString)
+
   try:
-    conn = pyodbc.connect(connectionString)
+    engine = sa.create_engine(connectionString)
+    Session = sessionmaker(engine)
+    session = Session()
   except Exception as e:
-    print(f'{colors.FAIL}[[ERROR]]: failed to connect to database.{colors.ENDC}')
-    print(f'\t{colors.FAIL}{e}{colors.ENDC}')
+    format_log(f'Failed to start database session.\n\t{e}', "error")
     return None
 
-  print(f'{colors.OKBLUE}[[INFO]]: successfuly connected to database.{colors.ENDC}')
-  return conn
+  format_log(f'successfuly connected to database.')
+  return session
   
+
+def get_data():
+  creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+  values = []
+  try:
+    service = build("sheets", "v4", credentials=creds)
+    sheet = service.spreadsheets()
+    values = (
+      sheet.values()
+           .get(spreadsheetId=SPREADSHEET_ID, range=RANGE)
+           .execute()
+           .get("values", []) )
+
+  except HttpError as err:
+    format_log(err, "error")
+    return None
+
+  return values
+
+
+def list_get(l, i):
+  try:
+    return l[i]
+  except:
+    return ''
+
+
+def apply_transforms(transforms, row):
+  return [ 
+    reduce((lambda acc, fn: fn(acc)) ,transforms.get(i, [lambda _:_]) + transforms[-1], c)
+    for i, c in enumerate([list_get(row, i) for i in range(17)]) # make sure all rows are same length
+  ]
+
 
 def main():
-  conn = open_connection()
-  if conn == None:
+  session = start_session(SERVER, DB, USER, PWD)
+  if session == None:
     exit(1)
-  
-  cursor = conn.cursor()
-  table_names = check_if_table_exists(cursor, INVENTORY_TABLE)
+
+  table_names = check_if_table_exists(session, VehicleInventory.__tablename__)
   if table_names == None:
     exit(2)
 
-  cursor = create_table(cursor, INVENTORY_TABLE, FIELDS) if len(table_names) == 0 else cursor
-  if cursor == None:
+  session = create_table(session, VehicleInventory.__table__) if len(table_names) == 0 else session
+  if session == None:
     exit(3)
 
-  conn.close()
+  rows = get_data()
+  if rows == None:
+    exit(4)
+
+  transformed_rows = [VehicleInventory(*apply_transforms(TRANSFORMS, r)) for r in rows]
+  insert_rows_into_table(session, VehicleInventory.__tablename__, transformed_rows[:])
+
+  session.close()
 
 
 if __name__ == "__main__":
